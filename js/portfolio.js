a8cce1ea6ddf1e817d3103780de43b7747d54ebe
onibenjo <onibenjo@gmail.com>
2019-02-10 18:32:25 +0100

    cleaned sample portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,36 +1,32 @@
 //Select DOM items
-const menuBtn = document.querySelector('.menu-button');
-const menu = document.querySelector('.menu');
-const menuBrand = document.querySelector('.menu-brand');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.nav-items');
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+const menuBrand = document.querySelector(".menu-brand");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-items");
 
 //State of menu
 let showMenu = false;
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    if(!showMenu){
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBrand.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBrand.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
 
-        //State of Menu
-        showMenu = true;
-    }
-    else{ 
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBrand.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
+    //State of Menu
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBrand.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
-        //State of Menu
-        showMenu = false;
-    }
+    //State of Menu
+    showMenu = false;
+  }
 }
-
-
-
